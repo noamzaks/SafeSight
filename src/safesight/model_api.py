b0ba88e40273a6d6b07307a12496235901df5544
Eyal Roginski <eyalrog1@gmail.com>
2024-05-28 19:31:26 +0300

    Added calculations for TestResults stats.

--- src/safesight/model_api.py
+++ src/safesight/model_api.py
@@ -5,7 +5,17 @@ from dataclasses import dataclass
 @dataclass
 class TestResults:
     """
-    See Typst doc for definitions of values.
+    Whereas:
+        * TP - # true positives
+        * TN - # true negatives
+        * FP - # false positives
+        * TP - # false negatives
+
+    Definitions:
+        * Accuracy = (TP + TN) / (TP + TN + FP + FN)
+        * Precision = (TP) / (TP + FP)
+        * False Positive Rate = (FP) / (FP + TN)
+        * False Negative Rate = (FN) / (FN + TP)
     """
 
     accuracy: float
@@ -15,9 +25,33 @@ class TestResults:
 
     def __init__(self, test_labels: list[bool], test_results: list[bool]) -> None:
         assert len(test_results) == len(test_labels)
-        self.accuracy = len(
-            [i for i in range(len(test_labels)) if test_labels[i] == test_results[i]]
+
+        tp = sum(
+            [int(label and result) for label, result in zip(test_labels, test_results)]
         )
+        tn = sum(
+            [
+                int(not label and not result)
+                for label, result in zip(test_labels, test_results)
+            ]
+        )
+        fp = sum(
+            [
+                int(not label and result)
+                for label, result in zip(test_labels, test_results)
+            ]
+        )
+        fn = sum(
+            [
+                int(label and not result)
+                for label, result in zip(test_labels, test_results)
+            ]
+        )
+
+        self.accuracy = (tp + tn) / (tp + tn + fp + fn)
+        self.precision = tp / (tp + fp)
+        self.false_positive_rate = fp / (fp + tn)
+        self.false_negative_rate = fn / (fn + tp)
 
 
 class Model:
