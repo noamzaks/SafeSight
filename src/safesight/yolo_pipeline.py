1063059da6f600e7dab5f25df831eff9f88fff16
omrisdeor <omrisdeor@mail.tau.ac.il>
2024-07-05 13:18:16 +0300

    created YOLO pipeline

---
+++ src/safesight/yolo_pipeline.py
@@ -0,0 +1,25 @@
+from PIL.Image import Image
+from ultralytics import YOLO
+
+from safesight.pipeline import Pipeline, Evaluation
+
+class YOLOPipeline(Pipeline):
+    def __init__(self, model_path: str, threshold: float = 0.5):
+        self.model = YOLO(model_path)
+        self.threshold = threshold
+
+    def process_image(self, image: Image) -> Evaluation:
+        results = self.model(image)[0]
+
+        for result in results.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = result
+
+            if score > self.threshold and results.names[int(class_id)].lower() == "accident":
+                return Evaluation(True)
+        return Evaluation(False)
+
+    def prepare(self):
+        pass
+
+    def cleanup(self):
+        pass
