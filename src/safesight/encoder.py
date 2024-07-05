681d0348f12c3f7823def55805ddd6339eb58789
Eyal Roginski <eyalrog1@gmail.com>
2024-07-05 11:44:07 +0300

    Some infrastructure to make encoder classifiers with different parameters

--- src/safesight/encoder.py
+++ src/safesight/encoder.py
@@ -1,13 +1,9 @@
 import sys
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Callable, Dict, List
+import sys
+from typing import Callable, Dict, List, Optional
 
-import PIL.Image
-import clip
-import torch
-import torch.utils.data
-import torchvision
 from PIL.Image import Image
 from torch import nn
 from torch import optim
@@ -43,12 +39,13 @@ class CLIPEncoder:
 
 class EncoderHead(nn.Module):
     def __init__(self, in_features):
-        self.layer1 = nn.Linear(in_features, 20)
-        self.layer2 = nn.Linear(20, 2)
+        super(EncoderHead, self).__init__()
+        self.layer1 = nn.Linear(in_features, 1)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x: torch.Tensor):
         x = self.layer1(x)
-        x = self.layer2(x)
+        x = self.sigmoid(x)
         return x
 
 
@@ -59,13 +56,27 @@ class EncoderClassifier:
     """
 
     def __init__(
-        self,
-        encoder: Callable[[Image], torch.Tensor] = CLIPEncoder("ViT-B/32").encode_image,
+        self, encoder: Callable[[Image], torch.Tensor], layer_sizes: List[int]
     ):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.encoder = encoder
         features = self._get_num_features()
-        self.head = nn.Sequential(nn.Linear(in_features=features, out_features=2))
+        if layer_sizes:
+            hidden_layers = []
+            for i in range(len(layer_sizes) - 1):
+                hidden_layers.append(nn.Linear(layer_sizes[i], layer_sizes[i + 1]))
+                hidden_layers.append(nn.ReLU())
+
+            self.head = nn.Sequential(
+                nn.Linear(features, layer_sizes[0]),
+                nn.ReLU(),
+                *hidden_layers,
+                nn.Linear(layer_sizes[-1], 1),
+                nn.Sigmoid(),
+            )
+        else:
+            self.head = nn.Sequential(nn.Linear(features, 1), nn.Sigmoid())
+        print(self.head)
         self.head.to(self.device)
 
     def _get_num_features(self):
@@ -106,7 +117,7 @@ class EncoderClassifier:
         Returns a dictionary from the numeric dataset labels (returned by evaluate_image) to the
         string labels (names of the subdirectories of dataset_path)
         """
-        criterion = nn.CrossEntropyLoss()
+        criterion = nn.BCELoss()
         optimizer = optim.SGD(
             self.head.parameters(),
             lr=settings.learning_rate,
@@ -127,12 +138,13 @@ class EncoderClassifier:
             for _, data in enumerate(trainloader, 0):
                 # get the inputs; data is a list of [inputs, labels]
                 inputs, labels = data
+                labels = labels.type(torch.float)
 
                 # zero the parameter gradients
                 optimizer.zero_grad()
 
                 # forward + backward + optimize
-                outputs = self.head(inputs)
+                outputs = self.head(inputs).squeeze()
                 loss = criterion(outputs, labels)
                 loss.backward()
                 optimizer.step()
@@ -162,9 +174,9 @@ class EncoderClassifier:
             for data in dataloader:
                 images, batch_labels = data
                 # calculate outputs by running images through the network
-                outputs = self.head(images)
-                # the class with the highest energy is what we choose as prediction
-                _, batch_predictions = torch.max(outputs.data, 1)
+                outputs = self.head(images).squeeze()
+                # round to 1 or 0 to get the predictions
+                batch_predictions = outputs.round()
                 batch_predictions = list(map(int, batch_predictions))
                 batch_labels = list(map(int, batch_labels))
 
@@ -176,7 +188,8 @@ class EncoderClassifier:
         return TestResults(labels, predictions)
 
     def save_head(self, path: Path):
-        torch.save(self.head, path)
+
+        torch.save(self.head.state_dict(), path)
 
     def load_head(self, path: Path):
         """
@@ -191,14 +204,84 @@ class EncoderClassifier:
         # file_camera = FileCamera(video_path)
 
 
+def train_and_test_encoder_classifier(
+    encoder: Callable[[Image], torch.Tensor],
+    training_settings: TrainingSettings,
+    layers: List[int],
+    train_dataset: Path,
+    test_dataset: Path,
+    save_model_path: Optional[Path],
+) -> TestResults:
+    model = EncoderClassifier(encoder, layers)
+    model.train_head(train_dataset, training_settings)
+    if save_model_path:
+        model.save_head(save_model_path)
+    return model.test(test_dataset)
+
+
 if __name__ == "__main__":
-    encoder = CLIPEncoder(
-        "ViT-L/14@336px", device="cuda" if torch.cuda.is_available() else "cpu"
+    print(
+        0,
+        train_and_test_encoder_classifier(
+            CLIPEncoder("ViT-B/32").encode_image,
+            TrainingSettings(learning_rate=0.01, momentum=0.9, epochs=7),
+            layers=[],
+            train_dataset=Path("zaksaset/train"),
+            test_dataset=Path("zaksaset/test"),
+            save_model_path=Path(
+                "encoder_models/Linear-B32-lr0.01-mom0.9-epochs7-zaksaset.pth"
+            ),
+        ),
     )
-    model = EncoderClassifier(encoder.encode_image)
-    model.train_head(
-        Path("zaksaset/train"),
-        TrainingSettings(learning_rate=0.01, momentum=0.9, epochs=10),
+    print(
+        1,
+        train_and_test_encoder_classifier(
+            CLIPEncoder("ViT-B/32").encode_image,
+            TrainingSettings(learning_rate=0.01, momentum=0.9, epochs=10),
+            layers=[50],
+            train_dataset=Path("zaksaset/train"),
+            test_dataset=Path("zaksaset/test"),
+            save_model_path=Path(
+                "encoder_models/Hidden50-B32-lr0.01-mom0.9-epochs7-zaksaset.pth"
+            ),
+        ),
     )
-    print(model.test(Path("zaksaset/test")))
-    model.save_head(Path("modelhead2.pth"))
+    print(
+        2,
+        train_and_test_encoder_classifier(
+            CLIPEncoder("ViT-B/32").encode_image,
+            TrainingSettings(learning_rate=0.01, momentum=0.9, epochs=10),
+            layers=[50, 50],
+            train_dataset=Path("zaksaset/train"),
+            test_dataset=Path("zaksaset/test"),
+            save_model_path=Path(
+                "encoder_models/Hidden50-50-B32-lr0.01-mom0.9-epochs10-zaksaset.pth"
+            ),
+        ),
+    )
+    print(
+        3,
+        train_and_test_encoder_classifier(
+            CLIPEncoder("ViT-B/32").encode_image,
+            TrainingSettings(learning_rate=0.05, momentum=0.9, epochs=10),
+            layers=[50],
+            train_dataset=Path("zaksaset/train"),
+            test_dataset=Path("zaksaset/test"),
+            save_model_path=Path(
+                "encoder_models/Hidden50-B32-lr0.05-mom0.9-epoch7-zaksaset.pth"
+            ),
+        ),
+    )
+    print(
+        4,
+        train_and_test_encoder_classifier(
+            CLIPEncoder("ViT-B/32").encode_image,
+            TrainingSettings(learning_rate=0.05, momentum=0.9, epochs=20),
+            layers=[200, 200],
+            train_dataset=Path("zaksaset/train"),
+            test_dataset=Path("zaksaset/test"),
+            save_model_path=Path(
+                "encoder_models/Hidden200-200-B32-lr0.01-mom0.9-epochs20-zaksaset.pth"
+            ),
+        ),
+    )
